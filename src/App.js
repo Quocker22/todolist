0bf18fff21930ff278a19834d6fb646aef6c7c6c
QuocDinh <quochit879@gmail.com>
2022-06-10 01:44:00 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  
+  const [job, setJob] = useState('')
+  const [list, setList] = useState(()=>{
+    const getJobs = JSON.parse(localStorage.getItem("jobs"))
+    return getJobs ?? []
+  });
+  const [even, setEven] = useState(list)
+  const [place, getPlace] = useState('');
+
+  const handlelocostorage =(index)=>{
+          const getList = JSON.parse(localStorage.getItem("jobs"))
+          getList.splice(index,1);
+          setEven(()=>{
+              const jsons = JSON.stringify(getList)
+              localStorage.setItem("jobs",jsons)
+              setList(getList)
+          })
+  }
+
+  const handleList =()=>{
+    if(job === ''){
+      getPlace('Vui lòng nhập công việc')
+    }else{
+    setList(reps => {   
+      const saveJob = [...reps, job]
+      const jsonJob = JSON.stringify(saveJob)
+      localStorage.setItem("jobs",jsonJob)
+      return saveJob
+    })
+    setJob('')
+  }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{padding:20}}>
+      <input 
+      value={job}
+      onChange={e=>setJob(e.target.value)} placeholder ={place}/>
+      <button onClick={handleList}>add</button>
+      <ul>
+        <h4>Công việc cần làm hôm nay</h4>
+        {list.map((job, index)=>(
+          <div key={index} className="container">
+              <li className="JobsCss">{job}</li>
+              <span onClick={()=>handlelocostorage(index)} style={{cursor: "pointer"}}>x</span>
+          </div>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
+
 
 export default App;
